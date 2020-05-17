e8e274959c3e73c77f42c125efec1303341075ba
Gary Justin <7003114+gejustin@users.noreply.github.com>
2020-05-16 20:19:05 -0400

    Add basic structure for new site header

--- public/js/init.js
+++ public/js/init.js
@@ -30,116 +30,6 @@ skel.init({
 
 	}
 
-}, {
-
-	layers: {
-
-		
-
-		// Transform test.
-
-			transformTest: function() {
-
-
-
-				// Only use CSS transforms with touch devices.
-
-					return (skel.vars.isTouch);
-
-			
-
-			},
-
-
-
-		// Layers.
-
-		// Probably shouldn't mess with these (at least until I've finished the skel-layers documentation).
-
-			layers: {
-
-				topPanel: {
-
-					states: 'global wide normal narrow narrower mobile',
-
-					position: 'top-center',
-
-					side: 'top',
-
-					hidden: true,
-
-					animation: 'pushY',
-
-					width: '100%',
-
-					height: '75%',
-
-					html: '<nav data-action="navList" data-args="nav"></nav>',
-
-					clickToClose: true,
-
-					swipeToClose: false,
-
-					orientation: 'vertical'
-
-				},
-
-				topButton: {
-
-					states: 'global wide normal narrow narrower mobile',
-
-					position: 'top-center',
-
-					width: 120,
-
-					height: 50,
-
-					html: '<span class="toggle" data-action="toggleLayer" data-args="topPanel"></span>'
-
-				},
-
-				sidePanel: {
-
-					states: 'global wide normal narrow narrower',
-
-					position: 'top-left',
-
-					side: 'left',
-
-					hidden: true,
-
-					animation: 'revealX',
-
-					width: 250,
-
-					height: '100%',
-
-					html: '<nav data-action="navList" data-args="nav"></nav>',
-
-					clickToClose: true,
-
-					orientation: 'vertical'
-
-				},
-
-				sideButton: {
-
-					states: 'global wide normal narrow narrower',
-
-					position: 'top-left',
-
-					width: 100,
-
-					height: 60,
-
-					html: '<span class="toggle" data-action="toggleLayer" data-args="sidePanel"></span>'
-
-				}
-
-			}
-
-	}
-
 });
 
 
@@ -184,19 +74,19 @@ skel.init({
 
 			if (skel.vars.IEVersion < 10) {
 
-				
+
 
 				// formerize v1.0 | (c) n33 | n33.co @n33co | MIT
 
 					$.fn.formerize=function(){var _fakes=new Array(),_form = $(this);_form.find('input[type=text],textarea').each(function() { var e = $(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = $(this); var x = $($('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = $(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = $(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { $(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); $(this).find('select').val($('option:first').val()); $(this).find('input,textarea').each(function() { var e = $(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
 
-				
+
 
 				// Apply formerize all forms.
 
 					$('form').formerize();
 
-			
+
 
 			}
 
@@ -272,11 +162,11 @@ skel.init({
 
 				});
 
-			
+
 
 			}
 
-			
+
 
 	});
 
